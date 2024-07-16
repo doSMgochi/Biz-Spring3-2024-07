83b9bfe862c6d4cc34f609c16cde5577b871ee93
doSMgochi <aenarchi@gmail.com>
2024-07-16 16:41:03 +0900

    수업 마무리

--- SpMVC-010-Student/src/main/java/com/callor/student/controller/HomeController.java
+++ SpMVC-010-Student/src/main/java/com/callor/student/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.callor.student;
+package com.callor.student.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.callor.student.models.StudentVO;
+
 /**
  * Handles requests for the application home page.
  */
@@ -16,17 +18,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
 	// http://localhost:8080/student/ 로 request 가 오면
+	// http://localhost:8080/student 로 request 가 오면
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
 		String formattedDate = dateFormat.format(date);
-
 		model.addAttribute("serverTime", formattedDate);
-
 		return "home";
 	}
 
+	@RequestMapping(value = "/input", method = RequestMethod.GET)
+	public String input(Model model) {
+		return "student/input";
+	}
+	
+	@RequestMapping(value = "/input", method = RequestMethod.POST)
+	public String input(StudentVO studentVO) {
+		return "student/input";
+	}
 }
--- SpMVC-010-Student/src/main/webapp/WEB-INF/views/user/admin.jsp
+++ SpMVC-010-Student/src/main/webapp/WEB-INF/views/user/admin.jsp
@@ -7,21 +7,7 @@
 <%@ include file="/WEB-INF/views/include/head.jspf"%>
 <body>
 	<%@ include file="/WEB-INF/views/include/header.jsp"%>
-	<section class="main">
-		<h2>학생정보</h2>
-		<table>
-			<tr>
-				<th>학번</th>
-				<th>이름</th>
-				<th>학과</th>
-				<th>학년</th>
-				<th>전화</th>
-				<th>주소</th>
-			</tr>
-		</table>
-		<div>
-			<a href="${rootPath }/input">학생정보 등록</a>
-		</div>
-	</section>
+	<h1>관리자</h1>
+
 </body>
 </html>
